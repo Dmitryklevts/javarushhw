11cd449f4d5923347804c9a8fcefe9d886baede3
Dmitryklevts <dclevtsov@yandex.ru>
2024-06-03 21:20:42 +0300

    test

--- LJWP/Block4/src/Main.java
+++ LJWP/Block4/src/Main.java
@@ -1,8 +1,119 @@
+import java.sql.Time;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        //Exercises
+        //1.
+        boolean isCarnivore = true;
+
+        if (isCarnivore) {
+            System.out.println("Your Dino Is Carnivore");
+        }
+        else {
+            System.out.println("Your Dino is not carnivore");
+        }
+
+        //2.
+        String specie= "VELOCIRAPTOR";
+
+        switch (specie){
+            case ("T-REX"):
+                System.out.println("It's a T-REX");
+                break;
+            case ("VELOCIRAPTOR"):
+                System.out.println("It's a VELOCIRAPTOR");
+                break;
+            default:
+                System.out.println("UNDEFINED");
+        }
+
+        //3.
+        int age = 20;
+
+        if (age <=10){
+            System.out.println("You are too young to work with dinosaurs");
+        }
+        else if (age >= 10 && age <=20 ){
+            System.out.println("You can work with small dinos");
+        }
+        else {
+            System.out.println("You can work both with small and big dinos");
+        }
+
+        //4.
+
+        int safetyRating = 10;
+
+        if (safetyRating == 0){
+            System.out.println("Pizdez");
+        }
+        else if (safetyRating >0 && safetyRating <=5){
+            System.out.println("It's okay");
+        }
+        else {
+            System.out.println("Really good");
+        }
+
+        //5.
+
+        String size = "M";
+
+        switch (size){
+            case ("XS"):
+                System.out.println("XS");
+                break;
+            case ("S"):
+                System.out.println("S");
+                break;
+            case ("M"):
+                System.out.println("M");
+                break;
+            case ("L"):
+                System.out.println("L");
+                break;
+            case ("XL"):
+                System.out.println("XL");
+                break;
+        }
+
+        //6.
+        int weight = 500;
+
+        if (weight <= 500) {
+            System.out.println("50 kilo");
+        }
+        else {
+            System.out.println("100 kilo");
+        }
+
+        //7.
+        String jobTitle = "Manager";
+
+        switch (jobTitle){
+            case ("Manager"):
+                System.out.println("Upravlyai");
+                break;
+            case ("Cleaner"):
+                System.out.println("Chisti");
+                break;
+        }
+
+        //TODO: 4.8
+
+        int time = 9; // current time in 24-hour format
+
+        if (time >= 10 && time <= 19) {
+            System.out.println("The park is open for day visitors.");
+        } else {
+            System.out.println("The park is closed for day visitors.");
+        }
+
+
+
+/*
         int nLetters = 0;
         Scanner girl = new Scanner(System.in);
         System.out.print("Введите имя девушки -->");
@@ -28,6 +139,11 @@ public class Main {
                 break;
             default:
                 System.out.println("Ваша кошька нам неизвестна, извините");
+
+ */
+
+
+
         }
 
 
@@ -72,4 +188,4 @@ public class Main {
 
          */
     }
-}
+
